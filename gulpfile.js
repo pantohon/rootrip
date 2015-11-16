661e093601a0ecf5c6849bc9403c44b9bb4c63e6
dulkappa <dulkappa@gmail.com>
2015-11-16 22:06:21 +0900

    Corresponding to es6 using babelify
    - modified gulpfile.js and package.json to use es6.

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,38 @@ var gulp = require('gulp');
 var jade = require('gulp-jade');
 var browserify = require('browserify');
 var riotify = require('riotify');
+var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var plumber = require('gulp-plumber');
 var webserver = require('gulp-webserver');
 
 gulp.task('jade', function() {
-  gulp
+  return gulp
   .src('src/index.jade')
   .pipe(jade())
   .pipe(gulp.dest('dist/'))
-  ;
 });
 
 gulp.task('browserify', function() {
-  browserify({ entries: ['src/js/components.js'] })
-  .transform(riotify, { 'template': 'jade' })
+  return browserify({
+    entries: 'src/js/components.js',
+    debug: true
+  })
+  .transform(riotify, {
+    'type': 'es6',
+    'template': 'jade'
+  })
+  .transform(babelify)
   .bundle()
-  .pipe(source('components.js'))
+  .on("error", function(err) { console.log("Error: " + err.message); })
+  .pipe(source('bundle.js'))
   .pipe(gulp.dest('dist/js/'))
-  ;
 });
 
 gulp.task('images', function() {
   gulp
   .src('src/img/**/*')
   .pipe(gulp.dest('dist/images/'))
-  ;
 });
 
 gulp.task('webserver', ['browserify', 'jade', 'images'], function() {
@@ -39,10 +45,9 @@ gulp.task('webserver', ['browserify', 'jade', 'images'], function() {
       livereload: true
     })
   )
-  ;
 
   gulp.watch('src/index.jade', ['jade']);
-  gulp.watch('src/js/custom-tag/*', ['browserify']);
+  gulp.watch('src/js/**/*', ['browserify']);
 });
 
 gulp.task('default', ['webserver']);
