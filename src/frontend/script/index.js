256d45fba55f7a0bd51631c00290550648021d3f
dulkappa <dulkappa@gmail.com>
2016-08-21 00:04:51 +0900

    remove unnecessary requirement

--- src/frontend/script/index.js
+++ src/frontend/script/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var riot = require('riot');
-var request = require('superagent');
 
 require('./components/rootrip-root/rootrip-root.tag.jade');
 require('./components/rootrip-header/rootrip-header.tag.jade');
