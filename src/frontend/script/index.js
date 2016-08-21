284edd5c389c713df96f7964039e18e2eec8fe0c
dulkappa <dulkappa@gmail.com>
2016-08-21 15:59:04 +0900

    rename rootrip-spot-viewer from rootrip-contents

--- src/frontend/script/index.js
+++ src/frontend/script/index.js
@@ -5,6 +5,6 @@ var riot = require('riot');
 require('./components/rootrip-root/rootrip-root.tag.jade');
 require('./components/rootrip-header/rootrip-header.tag.jade');
 require('./components/rootrip-navbar/rootrip-navbar.tag.jade');
-require('./components/rootrip-contents/rootrip-contents.tag.jade');
+require('./components/rootrip-spot-viewer/rootrip-spot-viewer.tag.jade');
 
 riot.mount('*');
